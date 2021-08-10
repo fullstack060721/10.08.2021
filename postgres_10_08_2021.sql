273f31885bda9cbccd25e310ddc6475d2d067756
fullstack060721 <87032460+fullstack060721@users.noreply.github.com>
2021-08-10 18:03:48 +0300

    Add files via upload

---
+++ postgres_10_08_2021.sql
@@ -0,0 +1,540 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.3
+-- Dumped by pg_dump version 13.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: adminpack; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
+
+
+--
+-- Name: hello_world(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.hello_world() RETURNS character varying
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN
+            RETURN CONCAT('Hello ','World! ', ' ', current_timestamp);
+        END;
+    $$;
+
+
+ALTER FUNCTION public.hello_world() OWNER TO postgres;
+
+--
+-- Name: sp_sum(double precision, double precision); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.sp_sum(a double precision, b double precision) RETURNS double precision
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE
+            sum double precision := 0;
+        BEGIN
+            sum := a + b;
+            RETURN sum;
+        END;
+    $$;
+
+
+ALTER FUNCTION public.sp_sum(a double precision, b double precision) OWNER TO postgres;
+
+--
+-- Name: sp_sum_diff(integer, integer); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.sp_sum_diff(x integer, y integer, OUT the_sum integer, OUT the_diff integer) RETURNS record
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN
+            the_sum := sp_sum(x, y);
+            the_diff := x - y;
+        END;
+    $$;
+
+
+ALTER FUNCTION public.sp_sum_diff(x integer, y integer, OUT the_sum integer, OUT the_diff integer) OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: cars; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cars (
+    id bigint NOT NULL,
+    brand text NOT NULL,
+    model text NOT NULL,
+    year integer NOT NULL,
+    color text NOT NULL
+);
+
+
+ALTER TABLE public.cars OWNER TO postgres;
+
+--
+-- Name: cars_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.cars_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.cars_id_seq OWNER TO postgres;
+
+--
+-- Name: cars_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.cars_id_seq OWNED BY public.cars.id;
+
+
+--
+-- Name: customer; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.customer (
+    id bigint NOT NULL,
+    name text NOT NULL,
+    address text NOT NULL
+);
+
+
+ALTER TABLE public.customer OWNER TO postgres;
+
+--
+-- Name: customer_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.customer_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.customer_id_seq OWNER TO postgres;
+
+--
+-- Name: customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.customer_id_seq OWNED BY public.customer.id;
+
+
+--
+-- Name: employee; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employee (
+    id bigint NOT NULL,
+    name text NOT NULL,
+    age integer NOT NULL,
+    address text NOT NULL,
+    salary real NOT NULL
+);
+
+
+ALTER TABLE public.employee OWNER TO postgres;
+
+--
+-- Name: employee_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.employee ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.employee_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: flights; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.flights (
+    id bigint NOT NULL,
+    airline_company_id bigint,
+    origin_country_id bigint,
+    dest_country_id bigint,
+    departure_time date,
+    landing_time date,
+    remaining_tickets integer
+);
+
+
+ALTER TABLE public.flights OWNER TO postgres;
+
+--
+-- Name: flights_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.flights ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.flights_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id bigint NOT NULL,
+    product_name text NOT NULL,
+    total_price real DEFAULT 0 NOT NULL,
+    customer_id bigint
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.orders_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.orders_id_seq OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
+
+
+--
+-- Name: rent; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.rent (
+    id bigint NOT NULL,
+    customer_id bigint NOT NULL,
+    car_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.rent OWNER TO postgres;
+
+--
+-- Name: rent_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.rent_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.rent_id_seq OWNER TO postgres;
+
+--
+-- Name: rent_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.rent_id_seq OWNED BY public.rent.id;
+
+
+--
+-- Name: cars id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cars ALTER COLUMN id SET DEFAULT nextval('public.cars_id_seq'::regclass);
+
+
+--
+-- Name: customer id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.customer ALTER COLUMN id SET DEFAULT nextval('public.customer_id_seq'::regclass);
+
+
+--
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Name: rent id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rent ALTER COLUMN id SET DEFAULT nextval('public.rent_id_seq'::regclass);
+
+
+--
+-- Data for Name: cars; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.cars (id, brand, model, year, color) FROM stdin;
+1	Ferrari	Testa rossa	2021	red
+\.
+
+
+--
+-- Data for Name: customer; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.customer (id, name, address) FROM stdin;
+1	uri	tel aviv
+2	danna	hertzellya
+3	moshe	new york
+\.
+
+
+--
+-- Data for Name: employee; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.employee (id, name, age, address, salary) FROM stdin;
+9	Paul	32	California	20000
+10	Allen	25	Texas	15000
+11	Teddy	23	Norway	20000
+12	Mark	25	Rich-Mond 	65000
+13	David	27	Texas	85000
+14	Kim	22	South-Hall	45000
+15	James	25	Houston	10000
+19	James1	24	Houston	10000
+20	James12	24	Houston	10000
+21	James121	24	Houston	10000
+22	James1211	24	Houston	10000
+23	James12111	24	Houston	10000
+25	James121111	24	Houston	10000
+26	James1211111	24	Houston	10000
+27	James12111111	24	Houston	10000
+28	James121111111	24	Houston	10000
+29	James1211111111	24	Houston	10000
+\.
+
+
+--
+-- Data for Name: flights; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.flights (id, airline_company_id, origin_country_id, dest_country_id, departure_time, landing_time, remaining_tickets) FROM stdin;
+\.
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.orders (id, product_name, total_price, customer_id) FROM stdin;
+2	laptop	4000	1
+4	gpu card	3200	1
+5	perfume	80	2
+7	shirt	5	\N
+\.
+
+
+--
+-- Data for Name: rent; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.rent (id, customer_id, car_id) FROM stdin;
+2	3	1
+\.
+
+
+--
+-- Name: cars_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.cars_id_seq', 1, true);
+
+
+--
+-- Name: customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.customer_id_seq', 3, true);
+
+
+--
+-- Name: employee_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.employee_id_seq', 29, true);
+
+
+--
+-- Name: flights_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.flights_id_seq', 1, false);
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 7, true);
+
+
+--
+-- Name: rent_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.rent_id_seq', 2, true);
+
+
+--
+-- Name: cars cars_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cars
+    ADD CONSTRAINT cars_pk PRIMARY KEY (id);
+
+
+--
+-- Name: customer customer_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.customer
+    ADD CONSTRAINT customer_pk PRIMARY KEY (id);
+
+
+--
+-- Name: employee employee_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employee
+    ADD CONSTRAINT employee_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: flights flights_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.flights
+    ADD CONSTRAINT flights_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: orders orders_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pk PRIMARY KEY (id);
+
+
+--
+-- Name: rent rent_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rent
+    ADD CONSTRAINT rent_pk PRIMARY KEY (id);
+
+
+--
+-- Name: employee unique_name; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employee
+    ADD CONSTRAINT unique_name UNIQUE (name);
+
+
+--
+-- Name: cars_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX cars_id_uindex ON public.cars USING btree (id);
+
+
+--
+-- Name: customer_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX customer_id_uindex ON public.customer USING btree (id);
+
+
+--
+-- Name: customer_name_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX customer_name_uindex ON public.customer USING btree (name);
+
+
+--
+-- Name: orders_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX orders_id_uindex ON public.orders USING btree (id);
+
+
+--
+-- Name: orders orders_customer_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_customer_id_fk FOREIGN KEY (customer_id) REFERENCES public.customer(id);
+
+
+--
+-- Name: rent rent_cars_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rent
+    ADD CONSTRAINT rent_cars_id_fk FOREIGN KEY (car_id) REFERENCES public.cars(id);
+
+
+--
+-- Name: rent rent_customer_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rent
+    ADD CONSTRAINT rent_customer_id_fk FOREIGN KEY (customer_id) REFERENCES public.customer(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
